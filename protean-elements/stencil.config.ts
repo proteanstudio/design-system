58954fb888f8212f4c6643c654507cef20492463
Abdul Hamid <abdul@abdulmhamid.com>
2021-01-28 18:02:35 -0600

    minor build updates

--- protean-elements/stencil.config.ts
+++ protean-elements/stencil.config.ts
@@ -3,10 +3,10 @@ import { Config } from '@stencil/core';
 export const config: Config = {
     namespace: 'protean-elements',
     outputTargets: [
-        // {
-        //   type: 'dist',
-        //   esmLoaderPath: '../loader',
-        // },
+        {
+          type: 'dist',
+          esmLoaderPath: '../loader',
+        },
         // {
         //   type: 'dist-custom-elements-bundle',
         // },
