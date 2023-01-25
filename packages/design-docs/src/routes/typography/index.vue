c14e3623adc2c609fa4339f58fa80e6f0452bbd2
Abdul Hamid <abdul@abdulmhamid.com>
2023-01-25 01:11:52 -0500

    chore: convert root and main nav to composition api (design-docs)

--- packages/design-docs/src/routes/typography/index.vue
+++ packages/design-docs/src/routes/typography/index.vue
@@ -1,3 +1,5 @@
 <style lang="scss" src="./styles.scss"></style>
+<script setup lang="ts">
+    import CodeSnippet from '@/components/code-snippet/index.vue';
+</script>
 <template src="./template.html" />
-<script lang="ts" src="./scripts.ts" />
