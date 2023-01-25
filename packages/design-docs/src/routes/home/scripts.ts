2e1241d3c8bdb2bce458be15f7aa855af5477eb3
Abdul Hamid <abdul@abdulmhamid.com>
2023-01-25 13:31:35 -0500

    chore: update home route syntax (design docs)

--- packages/design-docs/src/routes/home/scripts.ts
+++
@@ -1,3 +0,0 @@
-import { Vue } from 'vue-class-component';
-
-export default class HomeRoute extends Vue {}
--- packages/design-docs/src/routes/typography/index.vue
+++ packages/design-docs/src/routes/typography/index.vue
@@ -1,5 +1,5 @@
 <style lang="scss" src="./styles.scss"></style>
 <script setup lang="ts">
-    import CodeSnippet from '@/components/code-snippet/index.vue';
+import CodeSnippet from '@/components/code-snippet/index.vue';
 </script>
 <template src="./template.html" />
